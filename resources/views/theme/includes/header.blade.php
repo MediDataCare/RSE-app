bfef02d23ce3f66198507037a13ceabcae17fc4c
GoncaloSantos99 <120812786+GoncaloSantos99@users.noreply.github.com>
2023-10-29 22:06:42 +0000

    Set Default Navbar

--- resources/views/theme/includes/header.blade.php
+++ resources/views/theme/includes/header.blade.php
@@ -7,17 +7,19 @@
             <h1>MediData<span>Care<i class="fas fa-plus-circle"></i></span></h1>
             <!-- Antigo Logotipo da Pagina <h1>MediData<span>Care+</span></h1>-->
         </a>
-        <nav id="navbar" class="navbar w-md-100">
+        <!-- ALterado para Original <nav id="navbar" class="navbar w-md-100"> -->
+        <nav id="navbar" class="navbar">
             <i class="bi bi-list mobile-nav-toggle d-none"></i>
             <ul class="">
                 <li class="dropdown"><a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
                     <ul>
-                        <li><a href="/user/form">User</a></li>
-                        <li><a href="/entitie/form">Entitie</a></li>
+                        <li><a href="/user/form">Utilizador</a></li>
+                        <li><a href="/entitie/form">Entidade</a></li>
                     </ul>
                 </li>
                 <li><a class="nav-link scrollto" href="/contact">Contactos</a></li>
-                <a class="btn-getstarted scrollto w-md-100" href="/login">Login <i class="fas fa-user"></i></a>
+                <!-- <a class="btn-getstarted scrollto w-md-100" href="/login">Login <i class="fas fa-user"></i></a> -->
+                <a class="btn-getstarted scrollto" href="/login">Login <i class="fas fa-user"></i></a>
             </ul>
         </nav>
 
