fe142b949a99483adc7eb122cdcc75d70659f18b
Dinis Neves <dinis.neves@onesource.pt>
2023-11-15 22:28:49 +0000

    User profile - Lista de pedidos

--- resources/views/frontend/users/profile.blade.php
+++ resources/views/frontend/users/profile.blade.php
@@ -27,73 +27,57 @@
             @php
                 $exams = \App\Models\Exam::where('user_id', Auth::user()->id)->get();
                 $studies = \App\Models\Study::all();
-//                $studies = $studies->map(function ($item) use ($exams) {
-//                    $data = data_get($item, 'data.pending', []);
-//
-//                    // Use filter instead of map for filtering exams
-//                    $newExams = $exams->filter(function ($exam) use ($data) {
-//                        return in_array($exam->id, $data);
-//                    });
-//
-//                    // Add a new key to the item with the filtered exams
-//                    $item['filteredExams'] = $newExams;
-//
-//                    return $item;
-//                });
 
                 $allData = $studies->map(function ($study) use ($exams) {
-                    $studyExams = $exams->filter(function ($exam) use ($study) {
-                        return in_array($exam->id, data_get($study, 'data.pending'));
+                    $studyPendingExams = $exams->filter(function ($exam) use ($study) {
+                        return in_array($exam->id, data_get($study, 'data.pending', []));
+                    });
+                    $studyApprovedExams = $exams->filter(function ($exam) use ($study) {
+                        return in_array($exam->id, data_get($study, 'data.approved', []));
+                    });
+                    $studyRejectedExams = $exams->filter(function ($exam) use ($study) {
+                        return in_array($exam->id, data_get($study, 'data.rejected', []));
                     });
 
-                    return [
-                        'study_id' => $study->id,
-                        'exams' => $studyExams->toArray(),
-                    ];
+                    return ['pending' => $studyPendingExams->toArray(), 'approved' => $studyApprovedExams->toArray(), 'rejected' => $studyRejectedExams->toArray()];
+
                 });
             @endphp
-
-            <table class="table">
-                <thead>
-                <tr>
-                    {{--                    <th scope="col"></th>--}}
-                    <th scope="col">Estado</th>
-                    <th scope="col">Tempo restante</th>
-                    <th scope="col">Ação</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($studies ?? [] as $exam)
-                    @php
-                        $sexText = '-';
-                        $sexCode = data_get($exam->examOwner(), 'parameters.sex');
-                            if($sexCode == 'male')
-                                $sexText = 'Masculino';
-                            if($sexCode == 'female')
-                                $sexText = 'Feminino';
-                            if($sexCode == 'other')
-                                $sexText = 'Outro';
-                    @endphp
+            <div class="container mt-5 mb-5">
+                <table class="table">
+                    <thead>
                     <tr>
-                        <td>{{data_get($exam->examType()->first(), 'title', '-')}}</td>
-                        <td>{{data_get($exam->examOwner(), 'parameters.age', '-')}}</td>
-                        <td>{{$sexText}}</td>
-                        @php
-                            $state = 'Pendente';
-                                if(in_array($exam->id, data_get($study, 'data.pending'))){
-                                    $state = 'Pendente';
-                                }elseif(in_array($exam->id, data_get($study, 'data.approved'))){
-                                    $state = 'Aprovado';
-                                }elseif(in_array($exam->id, data_get($study, 'data.rejected'))){
-                                    $state = 'Rejeitado';
-                                }
-                        @endphp
-                        <td>{{ $state }}</td>
+                        <th scope="col">Dado</th>
+                        <th scope="col">Estado</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-
+                    </thead>
+                    <tbody>
+                    @foreach($allData ?? [] as $study)
+                        @foreach($study as $key => $value)
+                            @foreach($value as $exam)
+                                @php
+                                    $examType = \App\Models\ExamType::find(data_get($exam, 'exams_types_id'));
+                                @endphp
+                                <tr>
+                                    <td>{{data_get($examType, 'title', '-')}}</td>
+                                    @php
+                                        $state = 'Pendente';
+                                            if( $key ==='pending'){
+                                                $state = 'Pendente';
+                                            }elseif( $key ==='approved'){
+                                                $state = 'Aprovado';
+                                            }elseif( $key ==='rejected'){
+                                                $state = 'Rejeitado';
+                                            }
+                                    @endphp
+                                    <td>{{ $state }}</td>
+                                </tr>
+                            @endforeach
+                        @endforeach
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     @else
         <script>window.location = "/";</script>
